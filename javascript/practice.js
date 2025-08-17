b0e7e4ed1e469d17ccb85b465019c9898f181979
AslynFiona <aslynfiona123@gmail.com>
2025-08-17 11:39:14 +0530

    project

---
+++ javascript/practice.js
@@ -0,0 +1,10 @@
+var obj = {
+    name:"Fiona",
+    class:"ECE",
+    classmates:["Fio","Aslyn",67],
+    things:{mobile:"nokia",tab:"intel",numb:7}
+
+
+};
+delete obj.things["tab"];
+console.log(obj);
